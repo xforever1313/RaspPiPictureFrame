652ef5a59b7a4a2cecd0b561e9e51eabeb87eef2
Seth Hendrick <seth@shendrick.net>
2022-08-27 14:59:36 -0400

    Initial commit of web project.

---
+++ src/PiPictureFrame.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿//
+// PiPictureFrame - Digital Picture Frame built for the Raspberry Pi.
+// Copyright (C) 2022 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PiPictureFrame.Web.Models;
+
+namespace PiPictureFrame.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController( ILogger<HomeController> logger )
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
+        public IActionResult Error()
+        {
+            return View( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
+        }
+    }
+}
